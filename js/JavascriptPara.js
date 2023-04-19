8179ccc797c06fa31bf5bcad5aae829557f9354d
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2023-04-19 10:14:45 +0100

    Updated Parallax effect

---
+++ js/JavascriptPara.js
@@ -0,0 +1,6 @@
+const parallax = document.querySelector('.parallax');
+
+window.addEventListener('scroll', function() {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + 'px';
+});
